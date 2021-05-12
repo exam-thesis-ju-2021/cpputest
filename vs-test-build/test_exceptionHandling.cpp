2ebc1624fb190bcb4cd85b904bbf2c93f8bb8cd8
Jonatantoth <jonatantoth@hotmail.se>
2021-05-12 12:36:59 +0200

    exception handling done

---
+++ vs-test-build/test_exceptionHandling.cpp
@@ -0,0 +1,34 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+}
+
+TEST_GROUP(test_exceptionHandling)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+//Can not continue testing if unexpected exeption is thrown. 
+TEST(test_exceptionHandling, createException) {
+	try
+	{
+		//int i = *(int*)0;
+		throw "exception\n";
+	}
+	catch (...)
+	{
+		printf("exception catched");
+	}
+}
+
+TEST(test_exceptionHandling, verifyThatProgramKeptRunning) {
+	CHECK_EQUAL(1, 1);
+}
